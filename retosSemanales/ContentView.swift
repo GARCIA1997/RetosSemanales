dd788fa33db556a1cf72e01880fddeafd286983e
Alejandro Garcia <agalvarez@concredito.com.mx>
2024-05-27 12:48:12 -0600

    Initial Commit

---
+++ retosSemanales/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  retosSemanales
+//
+//  Created by Alejandro García on 27/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ retosSemanales/retosSemanalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  retosSemanalesApp.swift
+//  retosSemanales
+//
+//  Created by Alejandro García on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct retosSemanalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
