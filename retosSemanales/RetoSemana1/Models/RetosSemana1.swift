a5c32031a49186e62472fda7a1ef3bb9aa0728bf
Alejandro Garcia <agalvarez@concredito.com.mx>
2024-05-27 20:59:41 -0600

    first commit

---
+++ retosSemanales/RetoSemana1/Models/RetosSemana1.swift
@@ -0,0 +1,8 @@
+//
+//  RetosSemana1.swift
+//  retosSemanales
+//
+//  Created by Alejandro García on 27/05/24.
+//
+
+import Foundation
---
+++ retosSemanales/RetoSemana1/Views/HomeUIView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeUIView.swift
+//  retosSemanales
+//
+//  Created by Alejandro García on 27/05/24.
+//
+
+import SwiftUI
+
+struct HomeUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeUIView()
+}
